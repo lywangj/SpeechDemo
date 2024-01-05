931d36dfe5650fd0f70c7053dad2d15d65713dc5
LiyunOBS <liyun.wang.obs@gmail.com>
2024-01-05 17:24:38 +0000

    try to read welcome text

--- app/src/main/java/com/liyun/speechdemo/MainActivity.java
+++ app/src/main/java/com/liyun/speechdemo/MainActivity.java
@@ -26,6 +26,8 @@ import android.widget.TextView;
 import com.amazonaws.mobile.client.AWSMobileClient;
 import com.amazonaws.mobile.client.Callback;
 import com.amazonaws.mobile.client.UserStateDetails;
+import com.amazonaws.regions.Region;
+import com.amazonaws.regions.Regions;
 import com.amazonaws.services.polly.AmazonPollyPresigningClient;
 import com.amazonaws.services.polly.model.DescribeVoicesRequest;
 import com.amazonaws.services.polly.model.DescribeVoicesResult;
@@ -42,22 +44,8 @@ import java.util.List;
 public class MainActivity extends Activity {
     private static final String TAG = "PollyDemo";
 
-    private static final String KEY_SELECTED_VOICE_POSITION = "SelectedVoicePosition";
-    private static final String KEY_VOICES = "Voices";
-    private static final String KEY_SAMPLE_TEXT = "SampleText";
-    // SpinnerVoiceAdapter spinnerVoiceAdapter;
-    View progressBar;
-    // Media player
-    MediaPlayer mediaPlayer;
-    // Backend resources
-    private AmazonPollyPresigningClient client;
-    private List<Voice> voices;
-    // UI
-    private Spinner voicesSpinner;
-    private EditText sampleTextEditText;
-    private Button playButton;
-    private ImageButton defaultTextButton;
-    private int selectedPosition;
+    Polly pollyObject;
+    InputStream speechStream;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -65,225 +53,48 @@ public class MainActivity extends Activity {
         setContentView(R.layout.activity_main);
         Log.d(TAG, "onCreate: ");
 
-        // playButton = findViewById(R.id.readButton);
-        // progressBar = findViewById(R.id.voicesProgressBar);
-        // voicesSpinner = findViewById(R.id.voicesSpinner);
-        // defaultTextButton = findViewById(R.id.defaultTextButton);
-        // sampleTextEditText = findViewById(R.id.sampleText);
-
-        // progressBar.setVisibility(View.VISIBLE);
-
-//        if (savedInstanceState != null) {
-//            restoreInstanceState(savedInstanceState);
-//        }
-        // setupVoiceSpinner();
-        Log.d(TAG, "offffffffffff");
-        initPollyClient();
-        Log.d(TAG, "offffffffffff");
-        setupNewMediaPlayer();
-        Log.d(TAG, "offffffffffff");
-        // setupSampleTextEditText();
-        // setupDefaultTextButton();
-    }
-
-//    @Override
-//    protected void onSaveInstanceState(Bundle outState) {
-//        outState.putInt(KEY_SELECTED_VOICE_POSITION, voicesSpinner.getSelectedItemPosition());
-//        outState.putSerializable(KEY_VOICES, (Serializable) voices);
-//        outState.putString(KEY_SAMPLE_TEXT, sampleTextEditText.getText().toString());
-//
-//        super.onSaveInstanceState(outState);
-//    }
-
-//    void restoreInstanceState(Bundle savedInstanceState) {
-//        selectedPosition = savedInstanceState.getInt(KEY_SELECTED_VOICE_POSITION);
-//        voices = (List<Voice>) savedInstanceState.getSerializable(KEY_VOICES);
-//
-//        String sampleText = savedInstanceState.getString(KEY_SAMPLE_TEXT);
-//        if (sampleText.isEmpty()) {
-//            defaultTextButton.setVisibility(View.GONE);
-//        } else {
-//            sampleTextEditText.setText(sampleText);
-//            defaultTextButton.setVisibility(View.VISIBLE);
-//        }
-//    }
-
-    void initPollyClient() {
-        AWSMobileClient.getInstance().initialize(this, new Callback<UserStateDetails>() {
-            @Override
-            public void onResult(UserStateDetails result) {
-                // Create a client that supports generation of presigned URLs.
-                client = new AmazonPollyPresigningClient(AWSMobileClient.getInstance());
-                Log.d(TAG, "onResult: Created polly pre-signing client");
-
-                if (voices == null) {
-                    // Create describe voices request.
-                    DescribeVoicesRequest describeVoicesRequest = new DescribeVoicesRequest();
-
-                    try {
-                        // Synchronously ask the Polly Service to describe available TTS voices.
-                        DescribeVoicesResult describeVoicesResult = client.describeVoices(describeVoicesRequest);
-
-                        // Get list of voices from the result.
-                        voices = describeVoicesResult.getVoices();
-
-                        // Log a message with a list of available TTS voices.
-                        Log.i(TAG, "Available Polly voices: " + voices);
-                    } catch (RuntimeException e) {
-                        Log.e(TAG, "Unable to get available voices.", e);
-                        return;
-                    }
-                }
-
-//                runOnUiThread(new Runnable() {
-//                    @Override
-//                    public void run() {
-//                        spinnerVoiceAdapter.setVoices(voices);
-//                        voicesSpinner.setSelection(selectedPosition);
-//                        progressBar.setVisibility(View.INVISIBLE);
-//                        playButton.setEnabled(true);
-//                    }
-//                });
-            }
-
-            @Override
-            public void onError(Exception e) {
-                Log.e(TAG, "onError: Initialization error", e);
-            }
-        });
-        Log.d(TAG, "occccccccccccccccc");
-    }
-
-//    void setupSampleTextEditText() {
-//        sampleTextEditText.addTextChangedListener(new TextWatcher() {
-//            @Override
-//            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
-//            }
-//
-//            @Override
-//            public void onTextChanged(CharSequence s, int start, int before, int count) {
-//                defaultTextButton.setVisibility(sampleTextEditText.getText().toString().isEmpty() ?
-//                        View.GONE : View.VISIBLE);
-//            }
-//
-//            @Override
-//            public void afterTextChanged(Editable s) {
-//            }
-//        });
-//
-//        sampleTextEditText.setOnEditorActionListener(new TextView.OnEditorActionListener() {
-//            @Override
-//            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
-//                playButton.performClick();
-//                return false;
-//            }
-//        });
-//    }
-
-    void setupNewMediaPlayer() {
-        mediaPlayer = new MediaPlayer();
-        mediaPlayer.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
-            @Override
-            public void onCompletion(MediaPlayer mp) {
-                mp.release();
-                setupNewMediaPlayer();
-            }
-        });
-        mediaPlayer.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
-            @Override
-            public void onPrepared(MediaPlayer mp) {
-                mp.start();
-                playButton.setEnabled(true);
-            }
-        });
-        mediaPlayer.setOnErrorListener(new MediaPlayer.OnErrorListener() {
-            @Override
-            public boolean onError(MediaPlayer mp, int what, int extra) {
-                playButton.setEnabled(true);
-                return false;
-            }
-        });
-    }
-
-    String getSampleText(Voice voice) {
-        if (voice == null) {
-            return "";
-        }
-
-        String resourceName = "sample_" +
-                voice.getLanguageCode().replace("-", "_").toLowerCase() + "_" +
-                voice.getId().toLowerCase();
-        int sampleTextResourceId =
-                getResources().getIdentifier(resourceName, "string", getPackageName());
-        if (sampleTextResourceId == 0)
-            return "";
-
-        return getString(sampleTextResourceId);
-    }
-
-    void setDefaultTextForSelectedVoice() {
-        Voice selectedVoice = (Voice) voicesSpinner.getSelectedItem();
-        if (selectedVoice == null) {
-            return;
-        }
-
-        String sampleText = getSampleText(selectedVoice);
-
-        sampleTextEditText.setHint(sampleText);
-    }
-
-//    void setupDefaultTextButton() {
-//        defaultTextButton.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                sampleTextEditText.setText(null);
-//            }
-//        });
-//    }
-
-    public void playVoice(View view) {
-        playButton.setEnabled(false);
-
-        Voice selectedVoice = (Voice) voicesSpinner.getSelectedItem();
-
-        // String textToRead = sampleTextEditText.getText().toString();
         String textToRead = "one two three";
 
-        // Use voice's sample text if user hasn't provided any text to read.
-        if (textToRead.trim().isEmpty()) {
-            textToRead = getSampleText(selectedVoice);
+        pollyObject = new Polly(Region.getRegion(Regions.US_WEST_1));
+
+        welcomeUser(textToRead);
+//        fileUpload(textToRead);
+//        fileUpdated(textToRead);
+//        fileDelete(textToRead);
+//        fileDownload(textToRead);
+//        errorWithFileOperation(textToRead);
+//        reminderSet(textToRead);
+//        logOutUser(textToRead);
+
+    }
+
+    public void welcomeUser(String user)
+    {
+        String welcome="Welcome "+user;
+        try
+        {
+            speechStream= pollyObject.synthesize(welcome, OutputFormat.Mp3);
+
+//            AdvancedPlayer player = new AdvancedPlayer(speechStream,javazoom.jl.player.FactoryRegistry.systemRegistry().createAudioDevice());
+//
+//            player.setPlayBackListener(new PlaybackListener() {
+//                @Override
+//                public void playbackStarted(PlaybackEvent evt) {
+//                    System.out.println("Playback started");
+//
+//                }
+//
+//                @Override
+//                public void playbackFinished(PlaybackEvent evt) {
+//                    System.out.println("Playback finished");
+//                }
+//            });
+//
+//            player.play();
+
+        } catch ( IOException ex) {
+            Log.e(TAG, "welcome user");
         }
 
-        // Create speech synthesis request.
-        SynthesizeSpeechPresignRequest synthesizeSpeechPresignRequest =
-                new SynthesizeSpeechPresignRequest()
-                        // Set text to synthesize.
-                        .withText(textToRead)
-                        // Set voice selected by the user.
-                        .withVoiceId(selectedVoice.getId())
-                        // Set format to MP3.
-                        .withOutputFormat(OutputFormat.Mp3);
-
-        // Get the presigned URL for synthesized speech audio stream.
-        URL presignedSynthesizeSpeechUrl =
-                client.getPresignedSynthesizeSpeechUrl(synthesizeSpeechPresignRequest);
-
-        Log.i(TAG, "Playing speech from presigned URL: " + presignedSynthesizeSpeechUrl);
-
-        // Create a media player to play the synthesized audio stream.
-        if (mediaPlayer.isPlaying()) {
-            setupNewMediaPlayer();
-        }
-        mediaPlayer.setAudioStreamType(AudioManager.STREAM_MUSIC);
-
-        try {
-            // Set media player's data source to previously obtained URL.
-            mediaPlayer.setDataSource(presignedSynthesizeSpeechUrl.toString());
-        } catch (IOException e) {
-            Log.e(TAG, "Unable to set data source for the media player! " + e.getMessage());
-        }
-
-        // Start the playback asynchronously (since the data source is a network stream).
-        mediaPlayer.prepareAsync();
     }
 }
